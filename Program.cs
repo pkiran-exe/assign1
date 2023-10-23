b79a693a591a4d53f6a8e5a425aa6d134ac694ad
P KIRAN <147041511+pkiran-exe@users.noreply.github.com>
2023-10-23 17:35:03 +0530

    first change

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Retrieve
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                try
+                {
+                    string path = @"D:\KP\Practiceproject1\Retrieve\Private_School.txt";
+                    string[] lines2 =File.ReadAllLines(path);
+                    Console.WriteLine("The Students Data are \n");
+                    int index = 1;
+                    foreach (string line in lines2)
+                        Console.WriteLine($"  {index++} : " + line);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("All the Students data in the files are Displayed.");
+
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
